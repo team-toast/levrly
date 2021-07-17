f92d96aed239783d5090748e1dd703a25239bc74
Schalk Dormehl <schalk.dormehl@gmail.com>
2021-07-17 15:15:57 +0200

    More work done to fill in the basics of the LSP interface.

--- smart-contracts/levrly/contracts/LongShortPair.sol
+++ smart-contracts/levrly/contracts/LongShortPair.sol
@@ -16,89 +16,139 @@ contract ILongShortPairToken is
 {
     using SafeMath for uint;
 
+    enum Asset 
+    { 
+        Long, 
+        Short
+    }
+
     uint constant public ONE_PERC = 10 ** 16;
     uint constant public ONE_HUNDRED_PERC = 10 ** 18;
 
-    ERC20 public collateralToken;
-    ERC20 public debtToken;
+    ERC20 public longToken;
+    ERC20 public interestToken; // the AAVE interest token to which interest accrues
+    ERC20 public shortToken;
+    ERC20 public debtToken;     // the AAVE debt token to which short accrues
 
     // Ratios
     // The PERC suffix indicates that the ratio is expressed as a percentage
-    uint public redemptionRatioLimitPERC; 
+    uint public minRedemptionRatioPERC; 
     uint public lowerRatioPERC;
     uint public targetRatioPERC;
     uint public upperRatioPERC;
 
-    // Fees
+    // Leverage fees
     uint public automationFeePERC;
     uint public protocolFeePERC;
     uint public issuerFeePERC;
+    
+    // Trading fees
+    uint public withinRangeFeePERC; // the fee charged if the trade is beneficial to the target ratio
+    uint public aboveRangeFeePERC;  // 
+    uint public belowRangeFeePERC;
+    uint public panicRangeFeePERC;
 
     address public gulper;
 
-    function issueWithCollateral(address _receiver, uint _collateralAmount) public;
-    function issueWithDebt(address _receiver, uint _debtAmount) public;
-    function tradeCollatralForDebt(address _receiver, uint _collateralAmount, uint _expirationTime) public;
-    function tradeDebtForCollateral(address _receiver, uint _debtAmount, uint _expirationTime) public;
+    // can be issued with long, short, or interest tokens
+    function issue(Asset _assetSupplied, address _receiver, uint _amount) public;
+    // unsure if these should be hidden inside of the issue function
+    function issueWithLong(address _receiver, uint _longAmount) public;
+    function issueWithShort(address _receiver, uint _shortAmount) public;
+
+    // can redeem to long, short, or interest tokens
+    function redeem(Asset _assetRequested, address _issuer, uint _amount) public;
+    // unsure if these should be hidden inside of the redeem function
+    function redeemToLong(address _receiver, uint _amount) public;
+    function redeemToShort(address _receiver, uint _amount) public;
+    
+    // cannot trade long and interest tokens, can trade short and interest tokens or long and short tokens
+    function trade(Asset _inAsset, Asset _outAsset, uint _amountIn, unt _expirationTime) public;
+    // unsure if these should be hidden inside of the trade function
+    function tradeCollatralForShort(address _receiver, uint _longAmount, uint _expirationTime) public;
+    function tradeShortForLong(address _receiver, uint _shortAmount, uint _expirationTime) public;
+    
     function changeSettings(uint _redemptionRatioLimit, uint _lowerRatio, uint _targetRatio, uint _upperRatio) public;
 
     function getRatio() public view returns (uint _ratio);
-    function getCollateralInfo() 
+    function getInfo() 
         public 
         view 
         returns (
-            uint _collateralPrice, 
-            uint _collateralAmount, 
-            uint _debtPrice, 
-            uint _debtAmount,
-            uint _collateralDenomenatedDebt,
-            uint _excessCollateral);
+            uint _longPrice, 
+            uint _longAmount, 
+            uint _shortPrice, 
+            uint _shortAmount,
+            uint _longDenomenatedShort,
+            uint _excessLong);
     
-    function calculateIssuanceAmount(uint _amount) 
+    function calculateIssuanceAmount_UsingLong(uint _longAmount) 
         public
         view
         returns (
             uint _protocolFee,
             uint _automationFee,
             uint _issuerFee,
-            uint _actualCollateralAdded,
-            uint _accreditedCollateral,
+            uint _actualLongAdded,
+            uint _accreditedLong,
+            uint _tokensIssued);
+
+    function calculateIssuanceAmount_UsingShort(uint _shortAmount) 
+        public
+        view
+        returns (
+            uint _protocolFee,
+            uint _automationFee,
+            uint _issuerFee,
+            uint _actualLongAdded,
+            uint _accreditedLong,
             uint _tokensIssued);
     
-    function calculateRedemptionAmount(uint _amount)
+    function calculateRedemptionAmount_ForLong(uint _amount)
         public
         view
         returns (
             uint _protocolFee,
             uint _automationFee,
             uint _issuerFee,
-            uint _collateralRedeemed,
-            uint _collateralReturned);
-
-    // This function returns the exact amount of the debt token that would be returned for
-    // the given amount of the collateral token.
-    // It will not run if the collateralization ratio is below the target ratio.
-    function calculateTradeOutput_CollateralForDebt(uint _collateralAmount)
-        public  
-        view
-        returns (uint _debtAmount); 
+            uint _longRedeemed,
+            uint _longReturned);
     
-    // This function returns the exact amount of the collateral token that would be returned for
-    // the given amount of the debt token.
-    // It will not run if the collateralization ratio is above the target ratio.
-    function calculateTradeOutput_DebtForCollateral(uint _debtAmount)
+    function calculateRedemptionAmount_ForShort(uint _amount)
         public
         view
-        returns (uint _collateralAmount);
+        returns (
+            uint _protocolFee,
+            uint _automationFee,
+            uint _issuerFee,
+            uint _shortRedeemed,
+            uint _shortReturned);
+
+
+    // This function returns the exact amount of the short token that would be returned for
+    // the given amount of the long token.
+    // It will not run if the collateralization ratio is below the target ratio.
+    function calculateTradeOutput_LongForShort(uint _longAmount)
+        public  
+        view
+        returns (uint _shortAmount); 
+    
+    // This function returns the exact amount of the long token that would be returned for
+    // the given amount of the short token.
+    // It will not run if the collateralization ratio is above the target ratio.
+    function calculateTradeOutput_ShortForLong(uint _shortAmount)
+        public
+        view
+        returns (uint _longAmount);
 
     event Issued(
         address _receiver, 
-        uint _suppliedCollateral,
+        uint _suppliedLong,
         uint _protocolFee,
         uint _automationFee,
         uint _issuerFee,
-        uint _actualCollateralAdded,
-        uint _accreditedCollateral,
+        uint _actualLongAdded,
+        uint _accreditedLong,
         uint _tokensIssued);
     
     event Redeemed(
@@ -108,8 +158,15 @@ contract ILongShortPairToken is
         uint _protocolFee,
         uint _automationFee,
         uint _issuerFee,
-        uint _collateralRedeemed,
-        uint _collateralReturned);
+        uint _longRedeemed,
+        uint _longReturned);
+
+    event Trade(
+        Asset _inAsset,
+        Asset _outAsset,
+        address _receiver,
+        uint _inAmount,
+        uint _outAmount);
 
     event ChangedSettings(
         uint _redemptionRatioLimit,
